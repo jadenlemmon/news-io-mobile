40bb4ab950ab4a5184465b47e906447a3ff4df5e
jlemm45 <jadenlemmon@gmail.com>
2016-09-11 10:55:53 -0500

    login already

--- www/js/Controllers/WelcomeController.js
+++ www/js/Controllers/WelcomeController.js
@@ -6,26 +6,9 @@
 
     function welcomeController(snugfeedUserService,$location) {
 
-        //$scope.error = false;
-        //
-        //if(snugfeedUserService.getApiToken()) {
-        //    $location.path( "/feeds" );
-        //}
-        //
-        //$scope.submitLogin = function($event,login) {
-        //    $event.preventDefault();
-        //    snugfeedUserService.loginUser(login).then(function(resp) {
-        //        if(resp.data.api_token) {
-        //            snugfeedUserService.setApiToken(resp.data.api_token);
-        //            $location.path( "/feeds" );
-        //        }
-        //        else {
-        //            $scope.error = true;
-        //        }
-        //    },function() {
-        //        $scope.error = true;
-        //    });
-        //}
+        if(snugfeedUserService.getApiToken()) {
+            $location.path( "/feeds" );
+        }
 
         var vm = this;
 
